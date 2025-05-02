426557f72277a78ac3c4ad44a9cf77577fba76e3
AwmirInf <am2aarabi@gmail.com>
2025-05-02 20:38:53 +0330

    feat: add solution & test for 9. Palindrome Number LeetCode

---
+++ LeetCode/9. Palindrome Number/9. Palindrome Number/Solution.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace _9._Palindrome_Number
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string input = x.ToString();
+            int end = input.Length;
+            if (end == 0) return false;
+
+            for (int i = 0; i < end; i++)
+            {
+                if (input[i] != input[end-1-i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/9. Palindrome Number/PalindromeNumber_Test/Test.cs
@@ -0,0 +1,27 @@
+﻿using _9._Palindrome_Number;
+
+namespace PalindromeNumber_Test
+{
+    public class Test
+    {
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)] 
+        [InlineData(10, false)]
+        [InlineData(0, true)]
+        [InlineData(12321, true)]
+        [InlineData(123321, true)]
+        [InlineData(12345, false)]
+        public void IsPalindrome_ShouldReturnExpectedResult(int input, bool expected)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            bool result = solution.IsPalindrome(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
